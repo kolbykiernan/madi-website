f67b2363a23c4f5c0ead6b7669c66bccd6bd2fdf
Kolby Kiernan <kolbykiernan@gmail.com>
2024-07-15 18:22:58 -0700

    final portfolio

---
+++ components/about.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import React from 'react';
+import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
+
+
+export default function About() {
+  const { ref } = useSectionInView('About');
+
+  return (
+    <motion.section 
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100}}
+      animate={{ opacity: 1, y: 0}}
+      transition={{ duration: .175 }}
+      id="about"
+      ref={ref}
+    >
+        <SectionHeading>About me</SectionHeading>
+        <p className='mb-3'>
+        Born and raised in <span className="font-medium">Anaheim, CA</span>, 
+        I went to college at <span className="font-medium">San Diego State University</span>{" "}
+        where I pursued <span className="font-medium">Army ROTC</span>{" "} and graduated with a <span className="font-medium">bachelor's</span> in{" "}
+        <span className="font-medium">Economics</span>. I decided to pivot away from the military and worked in {" "}
+        <span className="font-medium">Health/Wellness</span>, <span className="font-medium">Distribution</span>,
+        and then <span className="font-medium">SaaS</span> and <span className="font-medium">Fintech</span>{" "}
+        when I started to be immersed by <span className="font-medium">software</span>{" "} 
+        that was making a difference for thousands of people. {" "}
+        <span className="font-medium">This sparked my passion</span> in the world of programming,
+        so I returned to <span className="font-medium">San Diego State</span> {" "}
+        to learn how to become a proficient <span className="font-medium">full-stack software developer</span>.{" "}
+        <span className="italic">My favorite part of programming</span>{" "} is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack is{" "}
+        <span className="font-medium"> JavaScript </span> specifically, {" "}
+        <span className="font-medium">React Native, React.js, Next.js, and Node.js</span>
+        . I am also familiar with <span className="font-medium">TypeScript</span>,{" "} 
+        <span className="font-medium">Python3</span>, and many other languages, frameworks, and databases. 
+      I am always looking to
+        learn new technologies and am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+        </p>
+        <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing beach volleyball 🏐,
+        distance running 🏃, lifting weights 🏋️, golfing🏌️, tennis 🎾, snowboarding 🏂, cooking 🧑‍🍳, camping 🏕️, lake trips 🚤, playing guitar 🎸, singing 🎵, and listening to records 🎧. I am also a{" "}
+        <span className="font-medium">Certified Specialist of Wine 🍷</span>, enjoy
+        learning about{" "}
+        <span className="font-medium">history 🗿, geography 🗺️, and general trivia 🧠,</span> and love spending time with my family 👨🏼👩🏼🐶.
+        </p>
+    </motion.section>
+  )
+}
---
+++ lib/data.ts
@@ -0,0 +1,171 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { LuGraduationCap } from "react-icons/lu";
+import The_Nest from "@/public/The_Nest.png";
+import Aora from "@/public/Aora.png";
+import Rest_Rant from "@/public/Rest-Rant.png";
+import Guitar_Guru from "@/public/Guitar_Guru.png";
+import My_Portfolio from "@/public/My_Portfolio.png";
+import { StaticImageData } from "next/image";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "San Diego State University",
+    location: "San Diego, CA",
+    description:
+      "Received a bachelor's in Economics. Extracurriculars included: Army ROTC and Sigma Alpha Epsilon fraternity.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2013-2017",
+  },
+  {
+    title: "Sales Coordinator @ Medikeeper",
+    location: "San Diego, CA",
+    description:
+      "Collaborated with the sales and marketing teams to source new business at a health and wellness SaaS startup.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2017 - 2018",
+  },
+  {
+    title: "Sales Rep -> District Manager @ Gallo Wine Co.",
+    location: "Los Angeles, CA",
+    description:
+      "Managed the sales and distribution of wine and spirits for a team of 12, covering 200 drug and grocery stores.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2018 - 2021",
+  },
+  {
+    title: "Account Executive -> Sales Engineer @ Boulevard",
+    location: "Los Angeles, CA",
+    description:
+      "Worked with the sales and engineering teams at a Series C beauty and wellness SaaS company to find and present technical solutions to prospects and customers, increasing retention and reducing turnover.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2021 - 2024",
+  },
+  {
+    title: "San Diego State University",
+    location: "Online",
+    description:
+      "While at Boulevard, I completed a full-stack Software Development Bootcamp, earning a certificate of completion. However, most of my knowledge comes from self-teaching through books, online courses, podcasts, and YouTube.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023 - 2024",
+  },
+  {
+    title: "The Nest",
+    location: "Remote",
+    description:
+      "While completing my program, I began freelance work on 'The Nest,' a wedding-focused app for a stealth startup, which I am currently developing. I am now seeking full-time employment.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023 - 2024",
+  },
+] as const;
+
+export type Project = {
+  title: string;
+  description: string;
+  tags: string[];
+  imageUrl: StaticImageData;
+  url?: string;  // Make url optional
+};
+
+export const projectsData: Project[] = [
+  {
+    title: "The Nest",
+    description:
+      "A web app to create and manage a wedding guest list by categorizing and assigning values for guests, then comparing that to venue capacity and desired attendance.",
+    tags: ["React", "Express.js", "Node.js", "PostgreSQL", "React-Bootstrap"],
+    imageUrl: The_Nest,
+    url: "https://welcome-to-the-nest.onrender.com/"
+  },
+  {
+    title: "Aora",
+    description:
+      "A mobile app that allows users to upload and view videos, bookmark favorites, and join a community of digital content sharing.", 
+
+    tags: ["React Native", "Animatable", "Expo", "NativeWind", "Appwrite"],
+    imageUrl: Aora,
+    url: "https://www.loom.com/share/2893ba797ca94f9cad732159f9fd48ac?sid=a8ca8ed4-1f07-4c4b-b10e-f6084c84470d"
+  },
+  {
+    title: "My Portfolio",
+    description:
+      "My portfolio was a fun project that I couldn't leave out!",
+    tags: ["React", "Next.js", "TypeScript", "Framer-Motion", "Tailwind CSS", "Vercel"],
+    imageUrl: My_Portfolio,
+  
+  },
+  {
+    title: "Guitar Guru",
+    description:
+      "A browser-based interactive experience where users learn the basics of music through piano and guitar.",
+    tags: ["TypeScript", "Sass", "HTML"],
+    imageUrl: Guitar_Guru,
+    url: "https://kolbykiernan.github.io/Guitar-Guru/"
+  },
+  {
+    title: "Rest-Rant",
+    description:
+      "A web app that allows users to share, rank, and comment on their favorite restaurants",
+    tags: ["React", "Express.js", "Node.js", "MongoDB", "Bcrypt"],
+    imageUrl: Rest_Rant,
+    url: "https://rest-rant-27u0.onrender.com/"
+  }
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "Sass",
+  "JavaScript",
+  "TypeScript",
+  "React Native",
+  "React",
+  "Redux",
+  "Next.js",
+  "Node.js",
+  "Express",
+  "Tailwind",
+  "Bootstrap",
+  "Material UI",
+  "Framer Motion",
+  "MongoDB", 
+  "PostgreSQL",
+  "Appwrite",
+  "Python3",
+  "Flask",
+  "AWS",
+  "Git",
+  "Vercel",
+  "Expo Go",
+  "Jest",
+  "Cybersecurity",
+  "Sales",
+  "Management",
+  "Public Speaking",
+] as const;
