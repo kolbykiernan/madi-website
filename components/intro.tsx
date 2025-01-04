c6a7564cf516851805fe795e7cee085d580b412d
Kolby Kiernan <kolby.kiernan@gethealthie.com>
2025-01-03 17:25:34 -0800

    Touched up intro button styling

--- components/intro.tsx
+++ components/intro.tsx
@@ -80,34 +80,40 @@ export default function Intro() {
                 delay: .1
             }}
         >
+        <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
             <Link 
                 href="#contact"
                 className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
                 onClick={() => {
                     setActiveSection("Contact");
                     setTimeOfLastClick(Date.now());
-                }}  
+                }}
             >
-                Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+                Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
             </Link>
+            
             <a 
-                className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-gray-300 dark:bg-white/10"
                 href="/Resume.pdf" download={true}
             >
-                Download Resume <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
+                Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
             </a>
-            <a 
-                className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                href="https://www.linkedin.com/in/kolby-kiernan" target="_blank"
-            >
-                <BsLinkedin />
-            </a>
-            <a 
-                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                href="https://github.com/kolbykiernan" target="_blank"
-            >
-                <FaGithubSquare />
-            </a>
+
+            <div className="flex gap-4">
+                <a 
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-gray-300 dark:bg-white/10 dark:text-white/60"
+                    href="https://www.linkedin.com/in/kolby-kiernan" target="_blank"
+                >
+                    <BsLinkedin />
+                </a>
+                <a 
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-gray-300 dark:bg-white/10 dark:text-white/60"
+                    href="https://github.com/kolbykiernan" target="_blank"
+                >
+                    <FaGithubSquare />
+                </a>
+            </div>
+        </div>
         </motion.div>
 
     </section>
