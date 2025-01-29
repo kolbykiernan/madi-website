4ebd6951c56a57df17d3192bf60280cf80d5f11c
Kolby Kiernan <kolbykiernan@gmail.com>
2025-01-28 16:43:38 -0800

    Updated intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -69,7 +69,7 @@ export default function Intro() {
             <span className="font-bold">Hello, I&apos;m Kolby Kiernan.</span> I&apos;m a {" "}
             <span className="font-bold">Full-Stack Developer</span> with {" "}
             <span className="font-bold">2 years</span> of experience. I specialize in {" "}
-            <span className="italic">JavaScript / TypeScript / React / React Native</span>.    
+            <span className="italic">TypeScript, React, GraphQL, and Ruby</span>.    
         </motion.h1>
         <motion.div 
             className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
